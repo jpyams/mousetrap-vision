29579c2958212d1c68dfb28aba65e27a20446cf4
John Peter Yamauchi <jyamauchi@gmail.com>
2017-04-03 16:08:10 -0500

    Working towards a working trainer

--- hog.py
+++ hog.py
@@ -1,5 +1,24 @@
 import numpy as np
 import cv2
+import glob
+
+face_cascade = cv2.CascadeClassifier('../opencv/data/haarcascades/haarcascade_frontalface_alt.xml')
 
 hd = cv2.HOGDescriptor((64,64), (16,16), (8,8), (8,8), 9)
 
+imgFiles = glob.glob("class5/*.png")
+imgFiles.sort()
+
+store = open("store/5.txt", "w")
+
+for imgFile in imgFiles:
+	img = cv2.imread(imgFile)
+	faces = face_cascade.detectMultiScale(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 1.3, 5)
+	(x,y,w,h) = faces[0]
+	rect = img[y:y+h, x:x+w]
+	rect = cv2.resize(rect, (64,64))
+	rect = cv2.cvtColor(rect, cv2.COLOR_RGB2GRAY)
+	descriptors = hd.compute(rect)
+	store.write(" ".join([str(i[0]) for i in descriptors]) + "\n")
+
+store.close()
